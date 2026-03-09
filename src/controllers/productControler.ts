c1e3dc4d99b1796fd7a9ceeb74c9becdcdf520cb
KvotheGuile <kvothejoestar@gmail.com>
2026-03-08 20:00:46 -0700

    feat deleteProdict func

--- src/controllers/productControler.ts
+++ src/controllers/productControler.ts
@@ -75,14 +75,54 @@ export const getProductById: RequestHandler = (req: Request, res: Response) => {
   }); 
 }; 
  
-// Update a Product by the id in the request 
-export const modifyProduct: RequestHandler = async (req: Request,res: Response) => { 
- 
-  
+
+///Modify product 
+export const modifyProduct:RequestHandler = (req: Request, res: Response) => { 
+  // Validate request 
+  if (!req.body) { 
+    return res.status(400).json({ 
+      status: "error", 
+      message: "Content can not be empty.", 
+      payload: null, 
+    }); 
+  } 
+
+// Save Product in the database 
+  Product.update({ ...req.body }, { where: { id: req.params.id } }) 
+  .then((isUpdated) => { 
+    if (isUpdated) { 
+      return res.status(200).json({ 
+        status: "success", 
+        message: "Product successfully updated", 
+        payload: { ...req.body }, 
+      }); 
+    } else { 
+      return res.status(500).json({ 
+        status: "error", 
+        message: "Something happened updating the product. ", 
+        payload: null, 
+    }); 
+   } 
+  }) 
+  .catch((err) => { 
+    res.status(500).json({ 
+      status: "error", 
+      message: "Something happened updating a product. " + err.message, 
+      payload: null, 
+  }); 
+}); 
 }; 
  
-// Delete a Product with the specified id in the request 
-export const deleteProduct: RequestHandler = async (req: Request,res: Response) => { 
- 
-  
+///Delete product
+export const deleteProduct: RequestHandler = async (req: Request, res: Response): Promise<void> => { 
+    const { id } = req.body; 
+    try { 
+      await Product.destroy({ where: { id } }); 
+      res.status(200).json({ message: "Product deleted" }); 
+    } catch (error) { 
+      res.status(500).json({ 
+        message: "Error deleting products", 
+        error, 
+      }); 
+    } 
 }; 
