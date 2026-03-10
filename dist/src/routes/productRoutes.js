46077f28bbdca9e886f4dde65b61edeaa6598e69
KvotheGuile <kvothejoestar@gmail.com>
2026-03-09 19:58:16 -0700

    test commit

--- dist/src/routes/productRoutes.js
+++ dist/src/routes/productRoutes.js
@@ -1,24 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const productController_1 = require("../controllers/productController");
 const productRouter = (0, express_1.Router)();
-productRouter.get('/getallproducts', (req, res) => {
-    res.send('Get a list of products');
-});
-productRouter.get('/getproduct/:id', (req, res) => {
-    res.send(`Get the product ${req.params.id}`);
-});
-productRouter.post('/createproduct/:id', (req, res) => {
-    res.send(`Create a new product with ID: ${req.params.id}`);
-    //res.send(`Create a new product with ID: ${req.body.id}`)  
-});
-productRouter.patch('/updateproduct/:id', (req, res) => {
-    res.send(`Update the product ${req.params.id}`);
-    //res.send(`Update the product ${req.params.id} with the values of ${req.body.name}, ${req.body.price} and ${req.body.stock}`)  
-});
-productRouter.delete('/deleteproduct/:id', (req, res) => {
-    res.send(`Deleting the product ${req.params.id}`);
-    //res.send(`Deleting the product ${req.body.id}`)  
-});
+productRouter.get('/getallproducts', productController_1.getAllProducts);
+productRouter.get('/getproduct/:id', productController_1.getProductById);
+productRouter.post('/createproduct', productController_1.createProduct);
+productRouter.patch('/updateproduct/:id', productController_1.modifyProduct);
+productRouter.delete('/deleteproduct', productController_1.deleteProduct);
 exports.default = productRouter;
 //# sourceMappingURL=productRoutes.js.map
